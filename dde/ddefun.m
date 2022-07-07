889f72126991f4e497e12bb5f4fbffd662780ca9
herocbn <92193912+herocbn@users.noreply.github.com>
2022-07-07 09:02:19 +0800

    Delete dde directory

--- dde/ddefun.m
+++
@@ -1,8 +0,0 @@
-function dydt = ddefun(t,y,Z) % equation being solved
-  ylag1 = Z(:,1);
-  ylag2 = Z(:,2);
-
-  dydt = [ylag1(1); 
-          ylag1(1)+ylag2(2); 
-          y(2)];
-end
--- dde/history.m
+++
@@ -1,5 +0,0 @@
- 
-function s = history(t) % history function for t <= 0
-  s = ones(3,1);
-end
- 
